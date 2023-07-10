5e1a9603f8fc57b67704179a47e1f490a1368bd8
Puneetphull <Puneetphull21@gmail.com>
2023-07-10 20:18:00 +0530

    add the cart List

---
+++ src/app/data/CartList.js
@@ -0,0 +1,43 @@
+import dentalimg3 from "../assets/productListImages/goby-jEwi-9on5IE-unsplash.jpg";
+import dentalmirror from "../assets/productListImages/Dental Mirror.jpeg";
+import dentalinterdental from "../assets/productListImages/Interdental Brushes.jpeg";
+import dentalNight from "../assets/productListImages/Dental Night Guard.jpeg";
+import dentalcleanser from "../assets/productListImages/Denture Cleanser Tablets.jpeg";
+export const cartlist = [
+    {
+        "item_id": 7859,
+        "sku": "Electric Toothbrush",
+        "qty": 1,
+        "name": "Electric Toothbrush",
+        "price": 99,
+        "product_type": "simple",
+        "quote_id": "Y6B7T8YA07o5usKKTQZqbXIz8fUo6npX",
+        "extension_attributes": {
+            "image_url": dentalimg3
+        }
+    },
+    {
+        "item_id": 7860,
+        "sku": "Interdental Brushes",
+        "qty": 1,
+        "name": "Interdental Brushes",
+        "price": 100,
+        "product_type": "simple",
+        "quote_id": "Y6B7T8YA07o5usKKTQZqbXIz8fUo6npX",
+        "extension_attributes": {
+            "image_url":dentalcleanser
+        }
+    },
+    {
+        "item_id": 7861,
+        "sku": "Denture Cleanser Tablets",
+        "qty": 1,
+        "name": "Denture Cleanser Tablets",
+        "price": 48,
+        "product_type": "simple",
+        "quote_id": "Y6B7T8YA07o5usKKTQZqbXIz8fUo6npX",
+        "extension_attributes": {
+            "image_url": dentalinterdental
+        }
+    }
+]
